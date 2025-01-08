7e4ffd6c444013d10d2fe2a317ca993f4d3f19a5
abubaker-51215 <233534@students.au.edu.pk>
2025-01-09 00:23:03 +0500

    Add files via upload

---
+++ 233534-Assignment 5/BlazorApp1/NewFolder/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApp1.NewFolder
+{
+    
+    public class CitiesRepository
+    {
+        private static List<string> cities =new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
